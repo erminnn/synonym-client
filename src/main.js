c565febfab230528b497999a9182ef43b45e1380
Ermin Omeragić <erminnn@hotmail.com>
2022-04-05 01:15:16 +0200

    Added primevue and eslint config and tested primevue components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import Dialog from "primevue/dialog";
+import Dropdown from "primevue/dropdown";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+
+import "primevue/resources/themes/nova-vue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.component("Dialog", Dialog);
+app.component("Dropdown", Dropdown);
+
+app.mount("#app");
