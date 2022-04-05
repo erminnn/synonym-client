2b909c53a1a4ad7f35f755d39ed03cdd52a98dce
Ermin Omeragić <erminnn@hotmail.com>
2022-04-05 17:09:43 +0200

    Added proxy for server, changed port

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+    devServer: {
+        port: '3012',
+        proxy: {
+            '^/api/': {
+                target: 'http://localhost:3000',
+                pathRewrite: { '^/api/': '/api/' },
+                changeOrigin: false,
+                logLevel: 'debug',
+            },
+        },
+    },
+};
