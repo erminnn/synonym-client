e3911f9c3f6057ee6bd3ce49f1560e1d3fdff419
Ermin Omeragić <erminnn@hotmail.com>
2022-04-06 00:16:21 +0200

    Added component for listing word synonyms

---
+++ src/components/WordSynonyms.vue
@@ -0,0 +1,29 @@
+<template>
+    <Card class="p-col-12 p-mt-3">
+        <template #title>{{ word }}</template>
+        <template #content>
+            <h3>Word synonyms</h3>
+            <div class="p-d-flex p-flex-wrap">
+                <div class="demo-container p-p-2" v-for="synonym in synonyms" :key="synonym">
+                    <Chip :label="synonym" class="custom-chip" />
+                </div>
+            </div>
+        </template>
+    </Card>
+</template>
+<script>
+import Chip from 'primevue/chip';
+export default {
+    components: { Chip },
+    props: {
+        synonyms: Array,
+        word: String,
+    },
+};
+</script>
+<style scoped>
+.custom-chip {
+    background: #f59e0b;
+    color: #ffffff;
+}
+</style>
