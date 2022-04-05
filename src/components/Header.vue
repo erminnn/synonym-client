af92367030ce39255efb5e6418f694b35e03ea44
Ermin Omeragić <erminnn@hotmail.com>
2022-04-05 22:23:16 +0200

    changed title and background color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="p-col-12 header">Ermin</div>
+    <div class="p-col-12 header">
+        <div class="p-d-flex">
+            <span class="p-mt-auto p-mb-auto title">Synonym search tool</span>
+        </div>
+    </div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
 .header {
-    background-color: #fb4337;
+    background-color: #a855f7;
     color: #ffffff;
+}
+.title {
+    font-size: 20px;
+    font-weight: bold;
 }
 </style>
